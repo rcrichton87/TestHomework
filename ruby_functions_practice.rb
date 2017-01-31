9b3b716bb2e2a3610b82e6eeeed4e1acddcc1b7c
Ross Crichton <rcrichton87@googlemail.com>
2017-01-31 16:04:11 +0000

    Completed the two month functions

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -44,8 +44,18 @@ def number_to_full_month_name(n)
       return "May"
     when 6
       return "June"
+    when 7
+      return "July"
+    when 8
+      return "August"
     when 9
       return "September"
+    when 10
+      return "October"
+    when 11
+      return "November"
+    when 12
+      return "December"
     else
       return "Error"
   end
@@ -55,10 +65,28 @@ def number_to_short_month_name(n)
   case n
     when 1
       return "Jan"
+    when 2
+      return "Feb"
     when 3
       return "Mar"
+    when 4
+      return "Apr"
+    when 5
+      return "May"
+    when 6
+      return "Jun"
+    when 7
+      return "Jul"
+    when 8
+      return "Aug"
     when 9
       return "Sep"
+    when 10
+      return "Oct"
+    when 11
+      return "Nov"
+    when 12
+      return "Dec"
     else
       return "Error"
   end
